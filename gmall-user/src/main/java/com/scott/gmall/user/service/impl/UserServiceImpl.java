47c7461095558df3262e4454d347c53a08ba2917
pkum5y <Yazhou_Zhang.sgmext@SAIC-GM.com>
2020-02-20 19:05:18 +0800

    初始化项目

---
+++ gmall-user/src/main/java/com/scott/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.scott.gmall.user.service.impl;
+
+
+import com.scott.gmall.bean.UmsMember;
+import com.scott.gmall.bean.UmsMemberReceiveAddress;
+import com.scott.gmall.service.UserService;
+
+import com.scott.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.scott.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+
+        return userMapper.selectAll();
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+
+        // 封装的参数对象
+        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+
+//       Example example = new Example(UmsMemberReceiveAddress.class);
+//       example.createCriteria().andEqualTo("memberId",memberId);
+//       List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
+
+        return umsMemberReceiveAddresses;
+    }
+}
