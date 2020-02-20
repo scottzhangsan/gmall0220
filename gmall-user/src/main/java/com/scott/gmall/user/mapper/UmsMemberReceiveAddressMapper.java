d4dc78dc7bde46c8fbf04565713715dff2652a1e
pkum5y <Yazhou_Zhang.sgmext@SAIC-GM.com>
2020-02-20 19:01:09 +0800

    初始化项目

---
+++ gmall-user/src/main/java/com/scott/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,7 @@
+package com.scott.gmall.user.mapper;
+
+import com.scott.gmall.bean.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress>{
+}
