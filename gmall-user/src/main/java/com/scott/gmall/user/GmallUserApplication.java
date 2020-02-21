1069b75053ea12d96b74cfbce8ef7da97e759f9f
scottzhangsan <34437975+scottzhangsan@users.noreply.github.com>
2020-02-21 10:04:33 +0800

    test

--- gmall-user/src/main/java/com/scott/gmall/user/GmallUserApplication.java
+++ gmall-user/src/main/java/com/scott/gmall/user/GmallUserApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+///////
 public class GmallUserApplication {
 
     public static void main(String[] args) {
